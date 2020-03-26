7b9fa58d851c4108202074073a23ff62d53fb22a
gzzang <zangguangzhi@qq.com>
2020-03-26 18:27:37 +0800

    OK

---
+++ indefinite_integral/indefinite_integral.py
@@ -0,0 +1,20 @@
+# @Time    : 2020/3/25 16:45
+# @Author  : gzzang
+# @File    : indefinite_integral
+# @Project : notebook
+
+import sympy as mp
+
+x = mp.Symbol('x')
+
+expression = mp.sqrt(1 + x * x)
+integrate_expression = mp.integrate(expression, x)
+another_integrate_expression = x * mp.sqrt(x*x + 1)/2 + mp.log(x + mp.sqrt(1 + x * x)) / 2
+
+print('Expressions are different.')
+print(integrate_expression)
+print(another_integrate_expression)
+
+print('Values are same. (x==3)')
+print(integrate_expression.evalf(subs={x: 3}))
+print(another_integrate_expression.evalf(subs={x: 3}))
