eee337f44ae4a4bbb36ec7f486400b9da7e1fb76
gzzang <zangguangzhi@qq.com>
2020-05-03 13:14:57 +0800

    添加matplotlib的canvas

---
+++ matplotlib/canvas.py
@@ -0,0 +1,10 @@
+# @Time    : 2020/5/3 13:13
+# @Author  : gzzang
+# @File    : canvas
+# @Project : notebook
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+fig.canvas.set_window_title('new_name')
+plt.show()
